eaa51053f1da34b83bfa1c9cf560e864ccff2663
Lirac <Liraclomex@yahoo.com>
2019-07-25 00:25:02 +0100

    Before store setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        real-world-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My priceless Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
-
 <script>
-import Logo from '~/components/Logo.vue'
-
+import EventCard from '~/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 export default {
   components: {
-    Logo
+    EventCard
+  },
+  head() {
+    // <-- property used by vue-meta to add header tags
+    return {
+      title: 'Event Listing - Real World Events' // <-- For our title tag
+    }
+  },
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await EventService.getEvents()
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({
+        statusCode: 503,
+        message: 'Unable to fetch events at this time'
+      })
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
